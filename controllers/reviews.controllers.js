06660ac0aedcd83215b37de5e7df3fb9923e5888
Belinda Turvey <119790281+belindat98@users.noreply.github.com>
2023-05-09 16:57:46 +0100

    remove console log

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -4,7 +4,6 @@ exports.getReviewById = (req, res, next) => {
   const review_id = req.params.review_id;
   selectReviewById(review_id)
     .then((review) => {
-      console.log(review);
       res.status(200).send({ review });
     })
     .catch((err) => {
