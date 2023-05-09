c148831d86f766f5ee5e28e6f7413fd63afc87ad
Belinda Turvey <119790281+belindat98@users.noreply.github.com>
2023-05-09 15:45:25 +0100

    add GET /api endpoint

---
+++ controllers/api.controllers.js
@@ -0,0 +1,11 @@
+const fs = require('fs/promises')
+
+exports.getAPI = (req, res, next) => {
+    fs.readFile(`${__dirname}/../endpoints.json`,"utf8")
+    .then((endpoints) => {
+      res.status(200).send(JSON.parse(endpoints));
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
