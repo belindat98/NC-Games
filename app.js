8baf2621b47f41a998637e333386eed82fa28039
Belinda Turvey <119790281+belindat98@users.noreply.github.com>
2023-05-09 14:45:47 +0100

    add error handling on app

--- app.js
+++ app.js
@@ -9,4 +9,8 @@ app.get("*", (req, res) => {
     res.status(404).send({msg: "Not found"})
 })
 
+app.use((err,req,res) => {
+    res.status(500).send({msg: "Internal Server Error"})
+})
+
 module.exports = { app };
