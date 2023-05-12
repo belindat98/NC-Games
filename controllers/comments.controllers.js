0a99d71ad6e8811a7217aa71084d911f1238600d
Belinda Turvey <119790281+belindat98@users.noreply.github.com>
2023-05-12 11:07:04 +0100

    formatting

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,7 +2,9 @@ const { removeComment } = require("../models/comments.models");
 
 exports.deleteComment = (req, res, next) => {
   const comment_id = req.params.comment_id;
-  removeComment(comment_id).then(() => {
-    res.sendStatus(204);
-  }).catch(next)
+  removeComment(comment_id)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch(next);
 };
