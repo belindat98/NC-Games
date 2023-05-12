0ce238b7c4941970c19177e08da60785143688cd
Belinda Turvey <119790281+belindat98@users.noreply.github.com>
2023-05-12 14:11:07 +0100

    add comment_count to response for GET review by review_id

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -3,7 +3,7 @@ const { checkExists } = require("../utils/utils");
 
 exports.selectReviewById = (review_id) => {
   return db
-    .query(`SELECT * FROM reviews WHERE review_id = $1`, [review_id])
+    .query(`SELECT reviews.*, COUNT(comment_id) AS comment_count FROM reviews LEFT JOIN comments ON reviews.review_id = comments.review_id WHERE reviews.review_id = $1 GROUP BY reviews.review_id`, [review_id])
     .then(({ rows }) => {
       const review = rows[0];
       if (!review) {
